0c8dae66a52db65878b4393b48af4f1bb96060ed
hassine h'maied <hassine h'maied>
2016-07-27 20:21:39 +0100

    add angular structure

---
+++ app/app.js
@@ -0,0 +1,23 @@
+angular.module('gtec', []);
+
+angular.module('gtec').controller('gCtrl',function ($scope) {
+    var $ctrl=this;
+
+    $ctrl.list = [
+    {firstname:'hassine'},
+    {lastname:'hmaied'},
+    {age:'43'},
+    {age:'26'},
+    {adresse:'kélibia'}
+    ];
+
+    $ctrl.name="salut";
+    $ctrl.ok="yes";
+});
+
+angular.module('gtec').controller('g1Ctrl',function ($scope) {
+    var $ctrl=this;
+    $ctrl.name="bonjour";
+    $ctrl.hass="menzah";
+});
+
